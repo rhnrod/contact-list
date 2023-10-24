a2426588defbf1f54e8abdaf66a8c6f199d2a712
Rhuan Emanuel <rhnrod@gmail.com>
2023-10-23 23:46:18 -0300

    feat: add dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'commitlint.config.js'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'commitlint.config.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
