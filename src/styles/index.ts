0269f4e6db5624757c106da3771d5404c701342a
Rhuan Emanuel <rhnrod@gmail.com>
2023-10-24 16:08:28 -0300

    style: modify global style and create color-font variables

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,11 +1,15 @@
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
+import { variaveis } from './variaveis'
 
 export const GlobalStyle = createGlobalStyle`
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: 'Noto Sans', sans-serif
+  font-family: ${variaveis.fontPrimary}
 }
+`
 
+export const Container = styled.div`
+  display: flex;
 `
---
+++ src/styles/variaveis.ts
@@ -0,0 +1,10 @@
+export const variaveis = {
+  avatarYellow: '#ffbe0b',
+  avatarOrange: '#fb5607',
+  avatarPink: '#ef2d56',
+  avatarGreen: '#69b578',
+  avatarBlue: '#3a86ff',
+
+  fontPrimary: 'Noto Sans, sans-serif',
+  fontSecondary: 'Poppins, sans-serif'
+}
